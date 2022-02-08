fe590fed2ca1bfa043df8be15c13e90976b2003d
fatima <fnbz3010@gmail.com>
2022-02-08 15:11:06 +0100

    cart component

---
+++ src/components/productItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import formatCurrency from "../util"
+
+const productItem = ({product,addItemToCart}) => {
+  return (
+    <li key={product._id}>
+    <div className="product">
+      <a href={"#" + product._id}>
+        <img src={product.image} alt={product.title} />
+        <p>{product.title}</p>
+      </a>
+      <div className="product-price">
+        <div>{formatCurrency(product.price)}</div>
+        <button className="button primary" onClick={()=>{addItemToCart(product)}}>
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  </li>
+  );
+};
+
+export default productItem;
