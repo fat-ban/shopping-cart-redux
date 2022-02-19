e7914905d9813b9a0931587d5f0a838eb91a60e9
Fatima Bandou <fnbz3010@gmail.com>
2022-02-19 14:49:26 +0100

    add redux to Filter

--- src/App.js
+++ src/App.js
@@ -23,54 +23,9 @@ export default class App extends Component {
     };
   }
 
-  //sort
-  sortProducts = (event) => {
-    //impl
-    const sort = event.target.value;
-    console.log(event.target.value);
-
-    this.setState({
-      sort: sort,
-      //slice create cory of state
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    });
-  };
-
-  //filter
-  filterProducts = (event) => {
-    //imp
-
-    if (event.target.value === "") {
-      this.setState({
-        size: event.target.value,
-        products: data.products,
-      });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-
-    console.log(this.state.products);
-  };
-
+  //sort in redux
+  //filter in redux
+  
   //addItem
   addItemToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
@@ -118,18 +73,9 @@ export default class App extends Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter
-                count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
-              ></Filter>
+              <Filter/>
 
-              <Products
-                products={this.state.products}
-                addItemToCart={this.addItemToCart}
-              />
+              <Products addItemToCart={this.addItemToCart}/>
             </div>
             <div className="sidebar">
               <Cart
